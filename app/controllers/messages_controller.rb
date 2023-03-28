ab122a8d53915ed159f911795454223dd79bc36c
nakulyadav16 <nyadav@grepruby.io>
2023-03-28 17:43:24 +0530

    Action cable done

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,15 +1,12 @@
 class MessagesController < ApplicationController
-
   def create
     @ticket = Ticket.find(params[:ticket_id])
     @message = @ticket.messages.create(message_params)
-    redirect_to ticket_path(@ticket)
+    ActionCable.server.broadcast "ticket_channel_#{@message.ticket_id}" ,{ data: @message}
   end
 
   private
-
   def message_params
     params.require(:message).permit(:content ,:user_id ,:ticket_id)
   end
-
 end
---
+++ app/javascript/channels/ticket_channel.js
@@ -0,0 +1,53 @@
+import consumer from "./consumer"
+
+$(document).on('turbolinks:load', () => { 
+  let url = window.location.href
+  console.log(url)
+
+  if ( url.indexOf("tickets/") != -1 )
+  {
+    const ticket_element = document.getElementById('ticket-id')
+    const ticket_id = Number(ticket_element.getAttribute('data-ticket-id'))
+
+    // console.log(consumer.subscriptions)
+    
+    consumer.subscriptions.subscriptions.forEach( (subscription) => {
+      consumer.subscriptions.remove(subscription)
+    })
+
+    // console.log(consumer.subscriptions)
+    consumer.subscriptions.create( { channel: "TicketChannel", ticket_id: ticket_id}, {
+      connected() {
+        console.log("connected to " + ticket_id)
+      },
+      
+      disconnected() {
+        console.log("disconnected from " + ticket_id)
+      },
+
+      received(data) {
+        console.log(data)
+        const user_element = document.getElementById('user-id')
+        const user_id = Number(user_element.getAttribute('data-user-id'))
+
+        let new_message;
+        if ( user_id === data.data.user_id){
+          new_message = '<div  style="display: block; text-align-last: end; margin:2px; ">' + 
+          '<p style="margin: 0px; background:skyblue;" > '+ 
+            data.data.content +  
+          '</p>'
+        }
+        else{
+          new_message = '<div  style="display: block; margin:2px ">' + 
+          '<p style="margin: 0px;">' + 
+            data.data.content 
+          '</p>'
+        }
+        const message_container = document.getElementById('messages')
+        message_container.innerHTML = message_container.innerHTML + new_message
+
+        $('#message_content').val('')
+      }
+    });
+  }
+})
