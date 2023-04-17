827b8e15995f776e81f0974c7a2e5a87c07fb715
nakulyadav16 <nyadav@grepruby.io>
2023-04-17 10:27:33 +0530

    deliver now

--- app/services/ticket_manager/send_mail_and_create_history.rb
+++ app/services/ticket_manager/send_mail_and_create_history.rb
@@ -13,7 +13,7 @@ module TicketManager
     attr_reader :ticket
 
     def send_mail_and_create_history 
-      TicketGenerationMailer.ticket_generation(ticket).deliver_later
+      TicketGenerationMailer.ticket_generation(ticket).deliver_now
       TicketHistory.create(ticket_id: ticket.id, user_id: ticket.assigned_to_id)
     end
   end
